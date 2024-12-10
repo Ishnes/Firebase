4afeb10e7a8651c304ef910799c66c7247eaf51c
Ishnes <marcelciesiolkiewicz210@gmail.com>
2024-12-10 20:16:26 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+ import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+ import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+ import { signOut } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+
 // Your web app's Firebase configuration
         // For Firebase JS SDK v7.20.0 and later, measurementId is optional
         const firebaseConfig = {
