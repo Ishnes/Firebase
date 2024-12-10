94fee363c697996839f9f97b4f679900946c1386
Ishnes <marcelciesiolkiewicz210@gmail.com>
2024-12-10 19:38:27 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-// Konfiguracja Firebase
-const firebaseConfig = {
-    apiKey: "TWOJE_API_KEY",
-    authDomain: "TWOJE_AUTH_DOMAIN",
-    projectId: "TWOJE_PROJECT_ID",
-    storageBucket: "TWOJE_STORAGE_BUCKET",
-    messagingSenderId: "TWOJE_MESSAGING_SENDER_ID",
-    appId: "TWOJE_APP_ID"
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCKlFlmR1C5y-n1Q8MFY06glJlnwP6oSo4",
+    authDomain: "test-ba24c.firebaseapp.com",
+    projectId: "test-ba24c",
+    storageBucket: "test-ba24c.firebasestorage.app",
+    messagingSenderId: "385819042329",
+    appId: "1:385819042329:web:337cac20ed1faa6c810088",
+    measurementId: "G-BTRJPB6L9T"
   };
-  
-  // Inicjalizacja Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
-  
-  // Funkcja zapisująca dane do Firestore
-  document.getElementById('saveDataBtn').addEventListener('click', function() {
-      db.collection('dane')
-        .add({
-            name: "Test",
-            age: 25
-        })
-        .then(() => {
-            alert("Dane zapisane!");
-        })
-        .catch((error) => {
-            console.error("Błąd zapisania danych: ", error);
-        });
-  });
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
   
