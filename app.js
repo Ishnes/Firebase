ef901ab425786738898a8da3c89c446a4f443c05
Ishnes <marcelciesiolkiewicz210@gmail.com>
2024-12-10 19:48:41 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
-
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCKlFlmR1C5y-n1Q8MFY06glJlnwP6oSo4",
-    authDomain: "test-ba24c.firebaseapp.com",
-    projectId: "test-ba24c",
-    storageBucket: "test-ba24c.firebasestorage.app",
-    messagingSenderId: "385819042329",
-    appId: "1:385819042329:web:337cac20ed1faa6c810088",
-    measurementId: "G-BTRJPB6L9T"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  
-  // Dodaj dane do Firestore
+// Dodaj dane do Firestore
 db.collection('users').add({
     name: 'Jan Kowalski',
     email: 'jan@example.com'
