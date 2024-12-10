b6dd4ef0370ebe8d53e5c4acbd34125b66199e37
Ishnes <marcelciesiolkiewicz210@gmail.com>
2024-12-10 19:44:43 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -21,3 +21,25 @@
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
   
+  // Dodaj dane do Firestore
+db.collection('users').add({
+    name: 'Jan Kowalski',
+    email: 'jan@example.com'
+  })
+  .then((docRef) => {
+    console.log("Dokument zapisany z ID: ", docRef.id);
+  })
+  .catch((error) => {
+    console.error("Błąd dodawania dokumentu: ", error);
+  });
+  
+  // Odczyt danych z Firestore
+db.collection('users').get()
+.then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+  });
+})
+.catch((error) => {
+  console.log("Błąd odczytu danych: ", error);
+});
